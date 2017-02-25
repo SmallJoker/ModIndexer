a7eaf2398f3e7d3427cc12c9bf05a7fdddbcd612
SmallJoker <mk939@ymail.com>
2017-02-25 23:48:28 +0100

    Huge rewrite Clean up the mess and use enums Move helper functions to Misc.cs

---
+++ Misc.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Text;
+
+namespace ModIndexer
+{
+	public static class Misc
+	{
+		// Including the forum number as value
+		public enum FETCH_TYPE
+		{
+			REL_MODS  = 11,
+			WIP_MODS  =  9,
+			REL_GAMES = 16,
+			WIP_GAMES = 50,
+			OLD_MODS  = 13
+		}
+
+		// The number in the database for the type
+		public enum DATA_TYPE
+		{
+			INVALID  = 0,
+			REL_MOD  = 1,
+			REL_MP   = 2,
+			WIP_MOD  = 3,
+			WIP_MP   = 4,
+			OLD_MOD  = 5,
+			REL_GAME = 6,
+			WIP_GAME = 7
+		}
+
+		// Convert special characters to HTML code
+		public static string EscapeXML(this string t)
+		{
+			char[] fromCr = { '"', '\'', '\\', '{', '}', '|', '%', ':', '<', '>' };
+			string[] toStr = { "&quot;", "&#39;", "&#92;", "&#123;", "&#125;", "&#124;", "&#37;", "&#58;", "&lt;", "&gt;" };
+			StringBuilder sb = new StringBuilder();
+
+			bool wasSpace = true;
+			for (int i = 0; i < t.Length; i++) {
+				char cur = t[i];
+
+				bool isSpace = char.IsWhiteSpace(cur);
+				if (wasSpace && isSpace)
+					continue;
+
+				// Cut off non-ASCII
+				if ((ushort)t[i] > 0xFF)
+					continue;
+
+				bool found = false;
+				for (int k = 0; k < fromCr.Length; k++) {
+					if (cur == fromCr[k]) {
+						sb.Append(toStr[k]);
+						found = true;
+						break;
+					}
+				}
+
+				wasSpace = isSpace;
+				if (!found)
+					sb.Append(cur);
+			}
+
+			return sb.ToString();
+		}
+
+		public static int IndexOf<T>(this T[] me, T value)
+		{
+			for (int i = 0; i < me.Length; i++)
+				if (me[i].Equals(value))
+					return i;
+
+			return -1;
+		}
+	}
+
+	class ForumData
+	{
+		public int topicId, userId, type;
+		public string title, userName, link;
+
+		public ForumData(int topicId, string title, int type, 
+			int userId, string userName, string link)
+		{
+			this.topicId  = topicId;
+			this.title    = title;
+			this.type     = type;
+			this.userId   = userId;
+			this.userName = userName;
+			this.link     = link;
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -8,93 +8,63 @@ using Newtonsoft.Json;
 
 namespace ModIndexer
 {
-	/*
-		Indev = 9,
-		Mods = 11,
-		Old = 13,
-		Games = 15
-	*/
-
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			Console.Title = "Minetest Mod Indexer - Let me index the stuff for you.";
 			Console.Write("Select a mode:\n" +
-					"\n\t1) MR titles" +
-					"\n\t2) MR topics" +
-					"\n\t3) WIP titles" +
-					"\n\t4) WIP topics" +
-					"\n\t5) Game topics" +
-					"\n\t6) Old Mods titles" +
-					"\n\t7) Exit" +
+					"\n\t1) Released mods  [full]" +
+					"\n\t2) WIP mods       [full]" +
+					"\n\t3) Released games [full]" +
+					"\n\t4) WIP games      [full]" +
+					"\n\t5) Old mods       [fast]" +
+					"\n\t6) Exit" +
 					"\n\nYour choice: ");
 
 			ConsoleKeyInfo k = new ConsoleKeyInfo();
-			while (k.Key != ConsoleKey.D1 &&
-				k.Key != ConsoleKey.D2 &&
-				k.Key != ConsoleKey.D3 &&
-				k.Key != ConsoleKey.D4 &&
-				k.Key != ConsoleKey.D5 &&
-				k.Key != ConsoleKey.D6) {
+			while (k.Key < ConsoleKey.D1 ||
+					k.Key > ConsoleKey.D6) {
+
 				k = Console.ReadKey(true);
 
-				if (k.Key == ConsoleKey.D7) return;
+				if (k.Key == ConsoleKey.D6)
+					return;
 			}
 			Console.WriteLine(k.KeyChar + "\n");
 
-			byte forum = 11;
-			bool fetch_topics = false;
-			if (k.Key == ConsoleKey.D2)
-				fetch_topics = true;
+			Misc.FETCH_TYPE forum;
 
-			if (k.Key == ConsoleKey.D3)
-				forum = 9;
-
-			if (k.Key == ConsoleKey.D4) {
-				forum = 9;
-				fetch_topics = true;
+			switch (k.Key) {
+			case ConsoleKey.D1:
+				forum = Misc.FETCH_TYPE.REL_MODS;
+				break;
+			case ConsoleKey.D2:
+				forum = Misc.FETCH_TYPE.WIP_MODS;
+				break;
+			case ConsoleKey.D3:
+				forum = Misc.FETCH_TYPE.REL_GAMES;
+				break;
+			case ConsoleKey.D4:
+				forum = Misc.FETCH_TYPE.WIP_GAMES;
+				break;
+			case ConsoleKey.D5:
+				forum = Misc.FETCH_TYPE.OLD_MODS;
+				break;
+			default:
+				Console.WriteLine("Error: Keys are not properly implemented!");
+				return;
 			}
-			if (k.Key == ConsoleKey.D5) {
-				forum = 15;
-				fetch_topics = true;
-			}
-			if (k.Key == ConsoleKey.D6)
-				forum = 13;
 
 			Console.Write("Start page: ");
 			string start = Console.ReadLine();
 			Console.Write("End page: ");
 			string stop = Console.ReadLine();
 
-			new Engine(forum, start, stop, fetch_topics);
+			new Engine(forum, start, stop);
 
 			Console.WriteLine("=== DONE!\nPress any key to exit.");
 			Console.ReadKey(false);
-		}
-	}
-
-	/* Database types
-	 1		mod
-	 2		modpack
-	 3		indev mod
-	 4		indev modpack
-	 5		old mod
-	 6		game
-	 */
-
-	class ForumData
-	{
-		public int topicId, userId, type;
-		public string title, userName, link;
-
-		public ForumData(int _topicId, string _title, int _type, int _userId, string _userName, string _link)
-		{
-			topicId = _topicId;
-			title = _title;
-			type = _type;
-			userId = _userId;
-			userName = _userName;
-			link = _link;
 		}
 	}
 
@@ -103,15 +73,15 @@ namespace ModIndexer
 		System.Text.Encoding enc = System.Text.Encoding.UTF8;
 		List<ForumData> update_data;
 		WebClient cli = new WebClient();
+		Misc.FETCH_TYPE forum;
 
-		bool fetch_topics;
-
-		public Engine(int forum, string start, string stop, bool _fetch_topics)
+		public Engine(Misc.FETCH_TYPE forum, string start, string stop)
 		{
+			this.forum = forum;
+
 			update_data = new List<ForumData>();
 			ServicePointManager.ServerCertificateValidationCallback += ValidateRemoteCertificate;
 
-			fetch_topics = _fetch_topics;
 			int begin, end;
 			if (!int.TryParse(start, out begin))
 				begin = 1;
@@ -120,7 +90,7 @@ namespace ModIndexer
 				end = 1;
 
 			for (int i = begin - 1; i < end; i++)
-				Start(i, forum);
+				FetchTopicList(i);
 
 			byte[] answer = Config.Upload(ref update_data);
 
@@ -137,13 +107,16 @@ namespace ModIndexer
 				}
 			} catch (Exception e) {
 				Console.WriteLine(e.ToString());
-				Console.WriteLine(enc.GetString(answer));
+				//Console.WriteLine(enc.GetString(answer));
 			}
 
 			Console.WriteLine("Done.");
 		}
 
-		bool ValidateRemoteCertificate(object sender, System.Security.Cryptography.X509Certificates.X509Certificate cert, System.Security.Cryptography.X509Certificates.X509Chain chain, System.Net.Security.SslPolicyErrors error)
+		bool ValidateRemoteCertificate(object sender,
+			System.Security.Cryptography.X509Certificates.X509Certificate cert,
+			System.Security.Cryptography.X509Certificates.X509Chain chain,
+			System.Net.Security.SslPolicyErrors error)
 		{
 			// If the certificate is a valid, signed certificate, return true.
 			if (error == System.Net.Security.SslPolicyErrors.None ||
@@ -190,29 +163,34 @@ namespace ModIndexer
 		}
 
 		// Analyze the topic list in a forum
-		void Start(int page, int forum)
+		void FetchTopicList(int page)
 		{
-			Console.WriteLine("=== Forum " + forum + " - Page " + (page + 1));
+			Console.WriteLine("=== Forum {0} ({1}) - Page {2}",
+				forum.ToString(), (int)forum, page + 1);
 
 			HtmlNodeCollection bodyNode = OpenPage(
-				"https://forum.minetest.net/viewforum.php?f=" + forum + "&start=" + (page * 30),
+				"https://forum.minetest.net/viewforum.php?f=" + (int)forum + "&start=" + (page * 30),
 				"//ul[@class='topiclist topics']//dt/a");
 
-			if (bodyNode == null) return;
+			if (bodyNode == null)
+				return;
 
-			bool canAdd = false;
+			bool get_author = false;
 			string title = "",
 				author = "";
 			int topicId = 0,
 				authorId = 0;
+
 			foreach (HtmlNode dtNode in bodyNode) {
 				#region extract IDs
 				string link = dtNode.GetAttributeValue("href", "");
 				link = link.Replace("&amp;", "&");
 				string[] linkArgs = link.Split('&');
-				if (!canAdd) {
+
+				if (!get_author) {
 					for (int i = 0; i < linkArgs.Length; i++) {
-						if (linkArgs[i].Length < 3) continue;
+						if (linkArgs[i].Length < 3)
+							continue;
 
 						string first = linkArgs[i][0].ToString() + linkArgs[i][1];
 						if (first == "t=") {
@@ -222,7 +200,8 @@ namespace ModIndexer
 					title = dtNode.InnerText;
 				} else {
 					for (int i = 0; i < linkArgs.Length; i++) {
-						if (linkArgs[i].Length < 3) continue;
+						if (linkArgs[i].Length < 3)
+							continue;
 
 						string first = linkArgs[i][0].ToString() + linkArgs[i][1];
 						if (first == "u=") {
@@ -232,54 +211,112 @@ namespace ModIndexer
 					author = dtNode.InnerText;
 				}
 				#endregion
-				if (canAdd && title.Length > 10) {
-					#region filter
-					byte type = 0; //not a mod
-					string lowTitle = title.ToLower();
-					if (lowTitle.StartsWith("[wip]")) {
-						bool end_space = (title[5] == ' ');
-						lowTitle = lowTitle.Remove(0, end_space ? 6 : 5);
-						title = title.Remove(0, end_space ? 6 : 5);
-					}
-					if (lowTitle.StartsWith("[mod]")) {
-						title = title.Remove(0, 5);
-						type = 1;
-					} else if (lowTitle.StartsWith("[modpack]")) {
-						title = title.Remove(0, 9);
-						type = 2;
-					} else if (lowTitle.StartsWith("[game]") &&
-							forum == 15) {
-						title = title.Remove(0, 6);
-						type = 6;
-					}
-					if (type == 0) {
-						canAdd = !canAdd;
-						continue;
-					}
+				if (!get_author || title.Length < 10)
+					goto flip;
+
+				#region filter
+				Misc.DATA_TYPE type = Misc.DATA_TYPE.INVALID;
+
+				string mod_name,
+					mod_tag;
+				title = parseTitle(title, out mod_name, out mod_tag);
+
+				switch (mod_tag) {
+				case "mod":
+				case "old mod":
 					switch (forum) {
-					case 9: type += 2; break; // make indev
-					case 13: type = 5; break; // is old
+					case Misc.FETCH_TYPE.REL_MODS:
+						type = Misc.DATA_TYPE.REL_MOD;
+						break;
+					case Misc.FETCH_TYPE.WIP_MODS:
+						type = Misc.DATA_TYPE.WIP_GAME;
+						break;
+					case Misc.FETCH_TYPE.OLD_MODS:
+						type = Misc.DATA_TYPE.OLD_MOD;
+						break;
+					default:
+						Console.WriteLine("INFO: Found a mod in the wrong place");
+						break;
 					}
-					#endregion
-					string mod_name = null;
-					title = specCharsToHex(removeTrash(title, out mod_name));
-					string download = "";
-					if (fetch_topics && type != 5) {
-						bool is_git = getLink(topicId, author, mod_name, ref download);
-						if (!(type <= 2 || type == 6 || is_git))
-							download = null;
+					break;
+				case "modpack":
+				case "old modpack":
+					switch (forum) {
+					case Misc.FETCH_TYPE.REL_MODS:
+						type = Misc.DATA_TYPE.REL_MP;
+						break;
+					case Misc.FETCH_TYPE.WIP_MODS:
+						type = Misc.DATA_TYPE.WIP_MP;
+						break;
+					case Misc.FETCH_TYPE.OLD_MODS:
+						type = Misc.DATA_TYPE.OLD_MOD;
+						break;
+					default:
+						Console.WriteLine("INFO: Found a modpack in the wrong place");
+						break;
 					}
-					update_data.Add(new ForumData(topicId, title, type, authorId, author, download));
-					author = "";
-					title = "";
+					break;
+				case "game":
+				case "subgame":
+					switch (forum) {
+					case Misc.FETCH_TYPE.REL_GAMES:
+						type = Misc.DATA_TYPE.REL_MP;
+						break;
+					case Misc.FETCH_TYPE.WIP_GAMES:
+					case Misc.FETCH_TYPE.WIP_MODS:
+						type = Misc.DATA_TYPE.WIP_MP;
+						break;
+					/*case Misc.FETCH_TYPE.OLD_GAMES:
+						type = Misc.DATA_TYPE.OLD_GAME;   // TODO
+						break;*/
+					default:
+						Console.WriteLine("INFO: Found a subgame in the wrong place");
+						break;
+					}
+					break;
 				}
 
-				canAdd = !canAdd;
+				if (type == Misc.DATA_TYPE.INVALID) {
+					Console.WriteLine("INFO: Don't know where to put this mod:" +
+						"\n\t(ID) Title: ({0}) {1}", topicId, title);
+					goto flip;
+				}
+				#endregion
+
+				string download = "";
+				if (forum != Misc.FETCH_TYPE.OLD_MODS) {
+					// Fetch topics, get download/source links
+					bool is_git = FetchSingleTopic(topicId, author, mod_name, ref download);
+
+					// TODO: Find an use for is_git
+				}
+
+				/*Console.WriteLine("Found mod: " + mod_name + 
+					"\n\tTag: " + mod_tag +
+					"\n\tLink: " + download +
+					"\n\tType: " + (int)type + " " + type.ToString());*/
+
+				update_data.Add(new ForumData(
+					topicId,
+					title.EscapeXML(),
+					(int)type,
+					authorId,
+					author,
+					download
+				));
+
+				// Empty for next fetch
+				author = "";
+				title = "";
+
+
+			flip:
+				get_author ^= true;
 			}
 		}
 
 		// Analyze topic contents and get link
-		bool getLink(int topicId, string author, string mod_name, ref string link)
+		bool FetchSingleTopic(int topicId, string author, string mod_name, ref string link)
 		{
 			Console.WriteLine("=== Topic " + topicId);
 			Thread.Sleep(200);
@@ -324,7 +361,7 @@ namespace ModIndexer
 					}
 
 					string text_lower = text.ToLower().Replace('-', '_');
-					if (text_lower.Contains(mod_name) && 
+					if (text_lower.Contains(mod_name) &&
 							number > forum_download) {
 
 						forum_download = number;
@@ -347,7 +384,7 @@ namespace ModIndexer
 							if (url[i] == '/') {
 								if (count == 4)
 									pos = i;
-								
+
 								count++;
 							}
 						}
@@ -356,7 +393,7 @@ namespace ModIndexer
 
 							// Try to find another link if it's not contained in the name
 							string src_lower = source.ToLower().Replace('-', '_');
-							if (src_lower.Contains(mod_name) || 
+							if (src_lower.Contains(mod_name) ||
 									(src_lower.Contains(author.ToLower()) && source == ""))
 								break;
 						}
@@ -392,13 +429,13 @@ namespace ModIndexer
 						source = url;
 
 					string src_lower = source.ToLower().Replace('-', '_');
-					if (src_lower.Contains(mod_name) || 
+					if (src_lower.Contains(mod_name) ||
 							(src_lower.Contains(author.ToLower()) && source == ""))
 						break;
 				}
 			}
-			if (source == "" && 
-				download == "" && 
+			if (source == "" &&
+				download == "" &&
 				forum_download == 0)
 				return false;
 
@@ -409,58 +446,28 @@ namespace ModIndexer
 			return source != "";
 		}
 
-		// Remove common control characters and spaces
-		string FastTrim(string t, string rm)
-		{
-			int index = 0;
-			bool space_begin = true;
-			char[] ret = new char[t.Length];
-
-			for (int i = 0; i < t.Length; i++) {
-				char cur = t[i];
-				bool found = false;
-
-				if (space_begin) {
-					space_begin = (
-						cur == ' ' ||
-						cur == '\t' ||
-						cur == '\n' ||
-						cur == '\r'
-					);
-					found = space_begin;
-				}
-				for (int k = 0; k < rm.Length && !found; k++) {
-					if (rm[k] == cur) {
-						found = true;
-						break;
-					}
-				}
-
-				if (!found) {
-					ret[index] = cur;
-					index++;
-				}
-			}
-			if (index < t.Length)
-				Array.Resize(ref ret, index);
-
-			return new string(ret);
-		}
-
 		// Remove useless tags from the forum titles
-		string bad_chars = ". !?";
-		string[] bad_prefix = { "minetest", "mod", "mods" };
-		string removeTrash(string t, out string mod_name)
+		const string MODNAME_ALLOWED_CHARS = "abcdefghijklmnopqrstuvwxyz0123456789_-";
+		// Content of [tags]
+		string[] bad_content = { "wip", "beta" };
+		// Beginnings of [tags]
+		string[] bad_prefix = { "minetest", "mod", "mods", "git" };
+		// [tags] to identify a mod
+		string[] identifiers = { "mod", "modpack", "game" };
+
+		string parseTitle(string title, out string mod_name, out string mod_tag)
 		{
-			mod_name = "<unknown>";
-			string raw = t;
-			int pos = -1,
+			mod_tag = "";
+			mod_name = "";
+
+			string raw = title;
+			int pos = 0,
 				open_pos = 0;
 			bool opened = false,
 				delete = false;
-			for (int i = 0; i < t.Length; i++) {
-				pos++;
-				char cur = t[i];
+
+			for (int i = 0; pos < raw.Length; i++, pos++) {
+				char cur = title[i];
 
 				if (cur == '[' || cur == '{') {
 					opened = true;
@@ -468,53 +475,75 @@ namespace ModIndexer
 					continue;
 				}
 				if (cur == ']' || cur == '}') {
+					// Tag closed
 					opened = false;
 
 					int len = pos - open_pos + 1;
-					string content = raw.Substring(open_pos + 1, len - 2).ToLower();
-					int num = 0xC0FFEE;
-					bool is_number = int.TryParse(content, out num);
+					string content = raw.Substring(open_pos + 1, len - 2).ToLower().Trim();
+					double num = 0.0f;
+					bool is_number = double.TryParse(content, out num);
 
-					if (delete || (num != 0xC0FFEE && is_number)
-						|| content == "git"
-						|| content == "github"
-						|| content == "wip"
-						|| content == "beta") {
+					if (!is_number && mod_tag == ""
+							&& identifiers.IndexOf(content) != -1) {
+						// Mod tag detected
+						mod_tag = content;
+						delete = true;
+					}
+
+					if (delete || is_number
+							|| bad_content.IndexOf(content) != -1
+							|| identifiers.IndexOf(content) != -1) {
+
+						// Remove this tag
 						raw = raw.Remove(open_pos, len);
 						pos -= len;
-					} else {
-						content = content.ToLower().Replace('-', '_');
-						int start_substr = 0;
-
-						foreach (string prefix in bad_prefix) {
-							if (content.Length <= start_substr + prefix.Length + 1)
-								break;
-
-							if (content.Substring(start_substr, prefix.Length) == prefix)
-								start_substr += prefix.Length;
-							
-							if (content[start_substr] == '_')
-								start_substr++;
-						}
-						
-						mod_name = content.Substring(start_substr);
+						delete = false;
+						continue;
 					}
+
+					content = content.Replace('-', '_');
+					int start_substr = 0;
+
+					foreach (string prefix in bad_prefix) {
+						if (content.Length <= start_substr + prefix.Length + 1)
+							break;
+
+						if (content.Substring(start_substr, prefix.Length) == prefix)
+							start_substr += prefix.Length;
+
+						if (content[start_substr] == '_')
+							start_substr++;
+					}
+
+					if (start_substr == 0) {
+						// Everything fine, nothing to replace
+						mod_name = content;
+					} else {
+						// Replace this tag with the proper name
+						mod_name = content.Substring(start_substr);
+						raw = raw.Remove(open_pos, len);
+						pos -= len;
+
+						string to_insert = "[" + mod_name + "]";
+						raw.Insert(pos, to_insert);
+						pos += to_insert.Length;
+					}
+
 					delete = false;
-					continue;
 				}
-				if (opened &&
-					!delete &&
-					strContains(bad_chars, cur)) {
+				if (opened && MODNAME_ALLOWED_CHARS.IndexOf(cur) == -1) {
 					delete = true;
 				}
 			}
 
 			delete = true;
 			pos = 0;
+
+			// Trim double whitespaces
 			char[] ret = new char[raw.Length];
 			for (int i = 0; i < raw.Length; i++) {
 				char cur = raw[i];
-				bool is_space = (cur == ' ');
+				bool is_space = char.IsWhiteSpace(cur);
 				if (delete && is_space)
 					continue;
 
@@ -532,46 +561,5 @@ namespace ModIndexer
 			return new string(ret);
 		}
 
-		// Convert special characters to HTML code
-		string specCharsToHex(string t)
-		{
-			char[] fromCr = { '"', '\'', '\\', '{', '}', '|', '%', ':', '<', '>' };
-			string[] toStr = { "&quot;", "&#39;", "&#92;", "&#123;", "&#125;", "&#124;", "&#37;", "&#58;", "&lt;", "&gt;" };
-			System.Text.StringBuilder sb = new System.Text.StringBuilder();
-
-			bool wasSpace = false;
-			for (int i = 0; i < t.Length; i++) {
-				bool isSpace = (t[i] == ' ');
-				if (wasSpace && isSpace)
-					continue;
-				// Cut off non-ASCII
-				if ((ushort)t[i] > 0xFF)
-					continue;
-
-				bool found = false;
-				for (int k = 0; k < fromCr.Length; k++) {
-					if (t[i] == fromCr[k]) {
-						sb.Append(toStr[k]);
-						found = true;
-						break;
-					}
-				}
-
-				wasSpace = isSpace;
-				if (!found)
-					sb.Append(t[i]);
-			}
-
-			return sb.ToString();
-		}
-
-		bool strContains(string t, char c)
-		{
-			for (int i = 0; i < t.Length; i++) {
-				if (t[i] == c)
-					return true;
-			}
-			return false;
-		}
 	}
 }
